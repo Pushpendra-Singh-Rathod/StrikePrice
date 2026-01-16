459b11e5746b849e11572567b0c797b1e4bcc3c9
unknown <rathodpushpendra06@gmail.com>
2026-01-16 17:00:32 +0530

    made changes

--- components/ui/navItems.tsx
+++ components/ui/navItems.tsx
@@ -1,12 +1,13 @@
 "use client";
 import { usePathname } from "next/navigation";
+import Link from "next/link";
 import navItems from "../../lib/constants";
 
 function NavItems() {
-  let pathname = usePathname();
+  const pathname = usePathname();
 
   const isActive = (path: string) => {
-    if (path == "/") return (pathname = "/");
+    if (path == "/") return pathname == "/";
 
     return pathname.startsWith(path);
   };
@@ -15,14 +16,14 @@ function NavItems() {
     <ul className="flex-col flex sm:flex-row p-2 gap-3 sm:gap-10 font-medium">
       {navItems.map((item) => (
         <li key={item.href}>
-          <a
+          <Link
             href={`${item.href}`}
             className={`hover:text-yellow-500 transition-colors ${
               isActive(item.href) ? "text-gray-100" : ""
             }`}
           >
             {item.title}
-          </a>
+          </Link>
         </li>
       ))}
     </ul>
